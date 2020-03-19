475b5f0de7d938b2d5666e07e99fb09aaf40719c
Kurshak <l.tisha@mail.ru>
2020-03-19 13:37:56 +0300

    Add user views Add bootstrap Add delay method for resize image

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,31 @@
+# frozen_string_literal: true
+
 class UserController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      @user.delay.resize_image if @user.avatar.attached?
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by_id(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :status, :email, :phone, :avatar)
+  end
 end
